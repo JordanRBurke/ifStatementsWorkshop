20e9505367cd0aab47cf4f7b12ec2d46439cb0b9
JordanRBurke <jordanrobertburke@gmail.com>
2018-01-26 13:57:47 -0500

    Finished Project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,78 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
+
+
+
     public static void main(String[] args) {
-	    
+        Scanner input = new Scanner(System.in);
+
+//        System.out.println("Lets play fizzbuzz!!!!! When a multiple of 3 pops up say Fizz if it is a multiple of 5 say buzz");
+
+
+//        int userInput = input.nextInt();
+//        if (userInput %3 == 0 && userInput %5 == 0) {
+//            System.out.println("FizzBuzz!");
+//
+//        } else if (userInput %3 == 0) {
+//            System.out.println("Fizz");
+//
+//            } else if (userInput %5 == 0) {
+//            System.out.println("Buzz");
+//
+//
+//        } else {
+//            System.out.println("Nope");
+
+
+
+
+//        }
+
+        //__________________ClassWork_______________________
+//        int userNumber = input.nextInt();
+
+//        for (int i = 1; i <= 100 ; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("Fizz Buzz");
+//            } else if (i % 3 == 0) {
+//
+//
+//        }
+
+//        int grade = input.nextInt();
+//        if (grade > 80) {
+//            System.out.println("A");
+//        } else if (grade < 80 && grade > 60) {
+//            System.out.println("You have a B!");
+//        } else if (grade < 80 && grade > 60) {
+//            System.out.println("You have a C!");
+//        } else if (grade < 49 && grade > 45) {
+//            System.out.println("You have a D!");
+//        } else if (grade < 45) {
+//            System.out.println("You have a F!");
+//        } else ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
 }
